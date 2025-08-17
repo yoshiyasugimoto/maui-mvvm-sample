a012c3c3c71c5242ccb0fbf1897125bbcee61c00
yoshiyasugimoto <gabesaiti@gmail.com>
2025-08-18 00:54:15 +0900

    feat: dotnet new

---
+++ mauiMvvmSample/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace mauiMvvmSample;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ mauiMvvmSample/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace mauiMvvmSample;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
